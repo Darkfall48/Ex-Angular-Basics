3e62aac82ffb60437868ad8a5ea4bcf90753ef67
Darkfall48 <sidney@sebban.be>
2023-02-17 08:29:16 +0200

    Challenge 1 - Completed!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-root/app.component';
+import { ShowTimeComponent } from './cmps/show-time/show-time.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ShowTimeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cmps/show-time/show-time.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'show-time',
+  templateUrl: './show-time.component.html',
+  styleUrls: ['./show-time.component.scss'],
+})
+export class ShowTimeComponent implements OnInit {
+  constructor() {}
+
+  clock: Date = new Date();
+  currSeason: string = 'Seasons';
+  isDark: boolean = true;
+  intervalId: any = null;
+  imgUrl = 'assets/imgs/seasons/seasons.png';
+
+  ngOnInit(): void {
+    this.getSeasonName();
+    this.setSeasonImg();
+    this.setTimer();
+  }
+  ngOnDestroy(): void {}
+
+  setTimer() {
+    this.intervalId = setInterval(() => {
+      this.clock = new Date();
+      // console.log('Clock', this.clock);
+    }, 1000);
+  }
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  onToggleIsDark() {
+    this.isDark = !this.isDark;
+  }
+  setIsDarkTitle() {
+    return `Change to ${this.isDark ? 'light' : 'dark'}`;
+  }
+
+  getSeasonName() {
+    const month = this.clock.getMonth();
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return (this.currSeason = 'Winter');
+      case 2:
+      case 3:
+      case 4:
+        return (this.currSeason = 'Spring');
+      case 5:
+      case 6:
+      case 7:
+        return (this.currSeason = 'Summer');
+      case 8:
+      case 9:
+      case 10:
+        return (this.currSeason = 'Autumn');
+      default:
+        return (this.currSeason = 'Invalid Month');
+    }
+  }
+  setSeasonImg() {
+    return (this.imgUrl = `assets/imgs/seasons/${this.currSeason.toLowerCase()}.png`);
+  }
+}
