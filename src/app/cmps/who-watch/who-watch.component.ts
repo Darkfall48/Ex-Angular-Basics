a7aa33635705ed6893850a0d6687f52bc06ac3a4
Darkfall48 <sidney@sebban.be>
2023-02-18 12:12:30 +0200

    Challenge 4 - Improved!

--- src/app/cmps/who-watch/who-watch.component.ts
+++ src/app/cmps/who-watch/who-watch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Watcher } from 'src/app/models/watcher.model';
 
 @Component({
   selector: 'who-watch',
@@ -10,43 +11,66 @@ export class WhoWatchComponent implements OnInit {
 
   isSelected: boolean = false;
   watcherSelected: any = null;
-  watchers = [
-    {
-      _id: 'w-101',
-      name: 'John Doe',
-      bgImage: 'blue',
-      shows: [
-        {
-          name: 'The Walking Dead',
-          imgUrl:
-            'https://fr.web.img5.acsta.net/c_310_420/pictures/22/08/29/18/20/3648785.jpg',
-        },
-        {
-          name: 'Simpsons',
-          imgUrl:
-            'https://fr.web.img3.acsta.net/c_310_420/pictures/20/10/01/11/26/1905965.jpg',
-        },
-      ],
-    },
-    {
-      _id: 'w-102',
-      name: 'Jane Doe',
-      bgImage: 'red',
-      shows: [
-        {
-          name: 'Naruto',
-          showUrl: 'https://www.imdb.com/title/tt0409591/',
-          imgUrl:
-            'https://fr.web.img6.acsta.net/c_310_420/pictures/19/08/02/15/12/4423178.jpg',
-        },
-        {
-          name: 'Futurama',
-          imgUrl:
-            'https://fr.web.img4.acsta.net/c_310_420/medias/nmedia/18/78/30/52/19784883.jpg',
-        },
-      ],
-    },
-  ];
+
+  watcher1 = {
+    _id: 'w-101',
+    name: 'John Doe',
+    bgImage: 'blue',
+    shows: [
+      {
+        name: 'The Walking Dead',
+        showUrl: 'https://www.imdb.com/title/tt1520211/',
+        imgUrl:
+          'https://fr.web.img5.acsta.net/c_310_420/pictures/22/08/29/18/20/3648785.jpg',
+      },
+      {
+        name: 'Simpsons',
+        showUrl: 'https://www.imdb.com/title/tt0096697',
+        imgUrl:
+          'https://fr.web.img3.acsta.net/c_310_420/pictures/20/10/01/11/26/1905965.jpg',
+      },
+    ],
+  };
+  watcher2 = {
+    _id: 'w-102',
+    name: 'Jane Doe',
+    bgImage: 'red',
+    shows: [
+      {
+        name: 'Naruto',
+        showUrl: 'https://www.imdb.com/title/tt0409591/',
+        imgUrl:
+          'https://fr.web.img6.acsta.net/c_310_420/pictures/19/08/02/15/12/4423178.jpg',
+      },
+      {
+        name: 'Futurama',
+        showUrl: 'https://www.imdb.com/title/tt0149460/',
+        imgUrl:
+          'https://fr.web.img4.acsta.net/c_310_420/medias/nmedia/18/78/30/52/19784883.jpg',
+      },
+    ],
+  };
+  watcher3 = {
+    _id: 'w-103',
+    name: 'Jaster Doe',
+    bgImage: 'green',
+    shows: [
+      {
+        name: 'You',
+        showUrl: 'https://www.imdb.com/title/tt7335184/',
+        imgUrl:
+          'https://fr.web.img5.acsta.net/c_310_420/pictures/22/11/25/09/18/2999342.jpg',
+      },
+      {
+        name: 'Re:Zero',
+        showUrl: 'https://www.imdb.com/title/tt5607616/',
+        imgUrl:
+          'https://fr.web.img3.acsta.net/c_310_420/pictures/20/04/17/15/18/4142036.jpg',
+      },
+    ],
+  };
+
+  watchers: Watcher[] = [this.watcher1, this.watcher2, this.watcher3];
 
   ngOnInit(): void {}
 
---
+++ src/app/models/watcher.model.ts
@@ -0,0 +1,6 @@
+export interface Watcher {
+  _id: string;
+  name: string;
+  bgImage: string;
+  shows: Array<object>;
+}
